64dc04ee881f76707b615edfa924c50403742fa4
Filbert Holmer <filbert.holmer@gmail.com>
2012-12-05 12:32:10 -0700

    First attempt at creating python.pak on the server

---
+++ pack-python.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Generate python.pak from python scripts in 'moul-scripts'
+# Wed Dec 5 MST 2012	F. Holmer	v0.0
+#
+SRC=$HOME/work/moul-scripts/Python
+DEST=$HOME/work/ServerStructure/auth/default/Python
+KEY=3b8c1dc62a3d951f14180a0c28841442	# default key
+
+# Make the .prc files. Be sure you are using python2.7
+cd $SRC
+rm -rf *.pyc
+python2.7 -m compileall .
+
+# create and secure the .pak file
+rm -f $DEST/python.pak $DEST/../Python.mbam
+PyPack -c -live $KEY system/*.pyc plasma/*.pyc xKI/*.pyc *.pyc $DEST/python.pak
+cd $DEST/..
+echo 'Python\python.pak' > Python.txt
+$HOME/work/make-mbam.pl Python.txt
+
+echo "Files generated:"
+echo "  $DEST/python.pak"
+echo "  `pwd`/Python.mbam"
+echo
+
+cd $HOME/work
